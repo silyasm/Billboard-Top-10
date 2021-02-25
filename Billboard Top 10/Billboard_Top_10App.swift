3f66d5b8ad20dfb9de4f91de6a99be6b2ef34e0e
Ilyas Munzir <ilyazmunzir@gmail.com>
2021-02-25 10:22:27 -0600

    Initial Commit

---
+++ Billboard Top 10/Billboard_Top_10App.swift
@@ -0,0 +1,17 @@
+//
+//  Billboard_Top_10App.swift
+//  Billboard Top 10
+//
+//  Created by Student on 2/25/21.
+//
+
+import SwiftUI
+
+@main
+struct Billboard_Top_10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Billboard Top 10/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Billboard Top 10
+//
+//  Created by Student on 2/25/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
